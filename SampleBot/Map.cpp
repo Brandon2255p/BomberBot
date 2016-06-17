a856ec029bc0dfb09d7bad28ad67ab53a620b1fa
Brandon Piner <brandon2255p@gmail.com>
2016-06-17 15:32:18 +0200

    Added find closest bomb functionality

--- SampleBot/Map.cpp
+++ SampleBot/Map.cpp
@@ -1,5 +1,5 @@
 #include "Map.h"
-
+#include <math.h>
 #include <iostream>
 
 cMapState::cMapState()
@@ -24,7 +24,7 @@ void cMapState::ClosestPowerUp(cLocation &ToThisLocation)
 			if (iter.GetIsPowerUp())
 			{
 				std::cout << "Power up at  X:" << iter.GetLocation().GetX() << " Y:" << iter.GetLocation().GetY() << std::endl;
-				std::cout << "Distance to player is: X" << iter.GetLocation().GetDistTo(ToThisLocation).GetX() << " Y:" << iter.GetLocation().GetDistTo(ToThisLocation).GetY();
+				std::cout << "Distance to player is: X" << iter.GetLocation().GetDistTo(ToThisLocation).GetX() << " Y:" << iter.GetLocation().GetDistTo(ToThisLocation).GetY() << std::endl;
 			}
 		}
 	}
@@ -32,6 +32,8 @@ void cMapState::ClosestPowerUp(cLocation &ToThisLocation)
 
 void cMapState::ClosestBomb(cLocation &ToThisLocation)
 {
+	int distance = 800;
+	cLocation ClosestBomb;
 	if (ToThisLocation.GetX() * ToThisLocation.GetY() > m_vMap.size())
 		std::cerr << "ClosestBomb::Coord out of range X:" << ToThisLocation.GetX() << " Y:" << ToThisLocation.GetY() << std::endl;
 	else
@@ -39,11 +41,19 @@ void cMapState::ClosestBomb(cLocation &ToThisLocation)
 		std::cout << "Player is at X:" << ToThisLocation.GetX() << " Y:" << ToThisLocation.GetY() << std::endl;
 		for (auto iter : m_vMap)
 		{
-			if (iter.GetIsPowerUp())
+			if (iter.GetIsBomb())
 			{
-				std::cout << "ClosestBomb at  X:" << iter.GetLocation().GetX() << " Y:" << iter.GetLocation().GetY() << std::endl;
-				std::cout << "Distance to player is: X" << iter.GetLocation().GetDistTo(ToThisLocation).GetX() << " Y:" << iter.GetLocation().GetDistTo(ToThisLocation).GetY();
+				int BombDist = sqrt(iter.GetLocation().GetDistTo(ToThisLocation).GetX() * iter.GetLocation().GetDistTo(ToThisLocation).GetX() + iter.GetLocation().GetDistTo(ToThisLocation).GetY() * iter.GetLocation().GetDistTo(ToThisLocation).GetY());
+				if (BombDist < distance)
+				{
+					distance = BombDist;
+					ClosestBomb.m_LocationX = iter.GetLocation().GetDistTo(ToThisLocation).GetX();
+					ClosestBomb.m_LocationY = iter.GetLocation().GetDistTo(ToThisLocation).GetY();
+				}
+				std::cout << "FoundBomb at  X:" << iter.GetLocation().GetX() << " Y:" << iter.GetLocation().GetY() << std::endl;
 			}
 		}
+		std::cout << "Closest bomb to player is: X" << ClosestBomb.GetX() << " Y:" << ClosestBomb.GetY() << " with a distance of " << distance << std::endl;
+
 	}
 }
